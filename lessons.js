408f0d32d4c5b849f2998a043eb13ac4fcce1322
harpal singh <hsmdxworks@gmail.com>
2022-11-16 12:37:35 +0500

    List usage

---
+++ lessons.js
@@ -0,0 +1,20 @@
+let lessons = [
+    { 
+        "id": 1101,
+        "subjectImg": "images/english.png",
+        "subjectName": "English",
+        "location": "London",
+        "price": 100,
+        "spaces": 10,
+        "currency": "£"
+    },
+    { 
+        "id": 102,
+        "subjectImg": "images/english.png",
+        "subjectName": "Mathematocs",
+        "location": "London",
+        "price": 190,
+        "spaces": 110,
+        "currency": "£"
+    },
+]
